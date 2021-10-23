1a563939a4ddf7f66f7d4e44d6ee3ba79ad561eb
Joao <joaojp003@gmail.com>
2021-10-22 22:21:15 -0300

    SOme final adjustments. Everything is working!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # photoToDrawing
+This program converts photos to images that look like drawings.
+I decided to make this program as a way to introduce myself to openCV and some basic commands.
+Run and test! There are some example images in the repository. Some generate better results than others. I will try to fix this in the future by treating the thresholds differently.
--- imgConvert.py
+++ imgConvert.py
@@ -1,16 +1,14 @@
 import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
 
 #location = '/Desktop/PersonalProjects/IMGtoDrawing/images'
-imgName = 'rockInRio.jpg'
+print("Hello! Type the name of your file(remember, it must be in the same directory as the program!):")
+imgName = input()
 
+print(imgName)
 #Read the image
 imgRead = cv.imread(imgName)
 
-cv.imshow('Original image', imgRead)
-
-#cConvert to gray
+#Convert to gray
 grayIMG = cv.cvtColor(imgRead, cv.COLOR_BGR2GRAY)
 #invert the image
 invertGray = 255 - grayIMG
@@ -21,22 +19,41 @@ blurred = cv.GaussianBlur(invertGray, (21,21), 0)
 #invert the blur
 invertBlur = 255 - blurred
 
-#Pencil img
+#Pencil image
 pencilIMG = cv.divide(grayIMG, invertBlur, scale=256.0)
 
 
-#clahe=cv.createCLAHE(clipLimit=40)
-#imgClahe=clahe.apply(imgContrast)
-th = 80
-max_val = 255
-
-# Basic threhold example
+# Basic threshold test
 imgContrast = cv.equalizeHist(pencilIMG)
 th, dst = cv.threshold(imgContrast, 26, 215, cv.THRESH_BINARY)
-cv.imshow('Final image', dst)
-cv.imshow('Original image', pencilIMG)
-cv.imshow('contrasted:',imgContrast)
 
-cv.waitKey(15000)
+cv.imshow('Drawn image:', pencilIMG)
+cv.waitKey(1000)
 
+print("Do you think your picture is clear? If not, wanna make it clearer?(y/n)")
+answer = input()
+if answer == 'y':
+    cv.imshow('contrasted:',imgContrast)
+    cv.waitKey(1000)
+    print("We've just increased the contrast of the picture."
+    "It may contain many black dots, making the viewing difficult.\n"
+    "If this is your case and you want us to try to fix it, press 'y'. If not, press 'n'.\n")
+    answer2 = input()
+
+    if answer2 == 'y':
+        cv.imshow('Final image', dst)
+        cv.waitKey(1000)
+        print("End of the program! Whenever ou want to exit, just press 'q':")
+        end = input()
+        if end == 'q':
+            cv.destroyAllWindows()
+            exit()
+    else:
+        cv.destroyAllWindows()
+        exit()
+else:
+    cv.destroyAllWindows()
+    exit()
+
+cv.waitKey(0)
 cv.destroyAllWindows()
